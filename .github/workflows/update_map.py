76c4e2bd960c9463068c0b3e6668ab62d4ab083a
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2023-01-19 00:02:17 +0300

    change: map updater completed

--- .github/workflows/update_map.py
+++ .github/workflows/update_map.py
@@ -1,33 +1,47 @@
-# change: map update
 import json
 import os
 from os import walk
 
+class Map:
+    folders = []
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
 root_path = ''
 
 def get_map():
-    map = { 'folders': '123' }
-    map = json.dumps(map)
-    f = get_folders(root_path)
-    for folder in f:
-        f.extend(get_folders(root_path + '/' + folder))
-    print(f)
-    print('\n')
-    print(map)
+    map = Map()
+    map.folders = get_folders(root_path)
+    return map.toJSON()
 
-def get_folders(path):
+def os_walk_folders(path):
     f = []
     for (dirpath, dirnames, filenames) in walk(path):
-        print(dirnames)
         f.extend(dirnames)
         break
-    print(f)
-    print('\n')
+    return f
+
+def get_folders(path):
+    result = []
+
+    for dir in os_walk_folders(path):
+        if '.' not in dir[0]:
+            group_folders = os_walk_folders(path + '/' + dir)
+            result.append({ 'name': dir, 'folders': group_folders })
+        print(dir)
+
+    return result
 
 def init():
-    print('Map update started!')
+    print('----------Map update started!-----------')
+    global root_path
     root_path = os.getcwd()
+    map = get_map()
+    map_file = open(root_path + '/map.json', 'w')
+    map_file.write(map)
+    map_file.close()
 
 if __name__ == '__main__':
     init()
-    get_map()
