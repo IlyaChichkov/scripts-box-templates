890b016ae97899b4e9aabeab6369973f8afdf3a1
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2023-01-18 23:20:16 +0300

    change: map update

--- .github/workflows/update_map.py
+++ .github/workflows/update_map.py
@@ -1,10 +1,20 @@
-
+# change: map update
 import os
+from os import walk
+
+root_path = ''
 
 def hello():
     print('Test started!')
-    print(os.getcwd())
+    root_path = os.getcwd()
     print(os.listdir())
+    print('\n')
+    f = []
+    for (dirpath, dirnames, filenames) in walk(root_path):
+        f.extend(filenames)
+        print(f)
+        print('\n')
+        break
     print('Test done!')
 
 if __name__ == '__main__':
